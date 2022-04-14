cb3c86cad015e5628e1e138d474cf0aacb00c488
Lindy Briggs <briggs.lindy@yahoo.com>
2022-04-14 14:53:56 -0500

    Refactored.

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 let searchButton = document.querySelector("#search")
 let cityInput = document.querySelector("#city")
 let historyArea = document.querySelector("#storeHistory")
-
+let searchCity = ""
 
 let cityHistory = [];
 if (localStorage.searchedCity !== undefined) {
     cityHistory = JSON.parse(localStorage.searchedCity)
 }
+saveHistory();
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -19,6 +20,7 @@ searchButton.addEventListener("click", function (event) {
 })
 
 function mainFunction() {
+    searchCity = cityInput.value
 
     console.log(cityInput.value)
 
@@ -29,10 +31,14 @@ function mainFunction() {
     console.log(localStorage.searchedCity);
     console.log(cityHistory);
 
+    fetchCityData(searchCity)
 
+}
+
+function fetchCityData(searchCity) {
     let apiKey = "5de95534471d1fc692031cdf2cecb3b3";
 
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=${apiKey}`)
         .then(response => response.json())
         .then(geoData => {
 
@@ -45,18 +51,17 @@ function mainFunction() {
 
             // console.log(cityData);
 
-            pullCurrentData(cityData);
+            pullCurrentData(cityData, searchCity);
             futureData(cityData);
 
         })
-
 }
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-function pullCurrentData(cityData) {
+function pullCurrentData(cityData, searchCity) {
 
-    // console.log(cityData);
+    console.log(cityData);
     // console.log(cityData.current.weather[0].icon)
     // console.log(cityData.current.temp);
     // console.log(cityData.current.wind_speed);
@@ -65,7 +70,7 @@ function pullCurrentData(cityData) {
 
     let currentDate = moment()
     let date = (currentDate.format('MMM Do YY'))
-    document.querySelector("#cityCurrent").innerText = cityInput.value + ", " + date;
+    document.querySelector("#cityCurrent").innerText = searchCity + ", " + date;
 
     let iconUrl = `<img src= "http://openweathermap.org/img/wn/${cityData.current.weather[0].icon}@2x.png"/>`
     // console.log(iconUrl)
@@ -78,8 +83,8 @@ function pullCurrentData(cityData) {
     let index = `<span id="uvColor" class="px-2 py-2 rounded">${cityData.current.uvi}</span>`
     $("#uv").append(index)
 
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+    // WHEN I view the UV index
+    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
     if (cityData.current.uvi >= 0 && cityData.current.uvi <= 2) {
         $("#uvColor").css("background-color", "green").css("color", "white");
     } else if (cityData.current.uvi >= 2 && cityData.current.uvi <= 5) {
@@ -150,7 +155,7 @@ function futureData(cityData) {
 }
 
 // Build search history using local storage from above
-function saveHistory(cityHistory) {
+function saveHistory() {
     historyArea.innerHTML = "";
 
     for (let i = 0; i < cityHistory.length; i++) {
@@ -171,53 +176,11 @@ function saveHistory(cityHistory) {
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-$(document).on("click", ".display", function(){
-        let buttonCity = $(this).text();
-        console.log(buttonCity);
-        reSearchFunction(buttonCity)
+$(document).on("click", ".display", function () {
+    let buttonCity = $(this).text();
+    console.log(buttonCity);
 
-        function reSearchFunction(buttonCity) {
+    fetchCityData(buttonCity)
 
-            let apiKey = "5de95534471d1fc692031cdf2cecb3b3";
-        
-            fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${buttonCity}&appid=${apiKey}`)
-                .then(response => response.json())
-                .then(geoData => {
-        
-                    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&exclude=hourly,minutely&units=imperial`)
-                })
-        
-                .then(response => response.json())
-                .then(cityData => {
-        
-                    rePullCurrentData(cityData);
-                    futureData(cityData);
-        
-                })
-        
-        }
-        function rePullCurrentData(cityData) {
-        
-            let currentDate = moment()
-            let date = (currentDate.format('MMM Do YY'))
-            document.querySelector("#cityCurrent").innerText = buttonCity + ", " + date;
-        
-            let iconUrl = `<img src= "http://openweathermap.org/img/wn/${cityData.current.weather[0].icon}@2x.png"/>`
-        
-            document.querySelector("#currentIcon").innerHTML = iconUrl
-            document.querySelector("#temp").innerText = "Temp: " + cityData.current.temp + "℉"
-            document.querySelector("#wind").innerText = "Wind: " + cityData.current.wind_speed + " MPH"
-            document.querySelector("#humidity").innerText = "Humidity: " + cityData.current.humidity + " %"
-            document.querySelector("#uv").innerText = "UV Index: "
-            let index = `<span id="uvColor" class="px-2 py-2 rounded">${cityData.current.uvi}</span>`
-            $("#uv").append(index)
-        
-            if (cityData.current.uvi >= 0 && cityData.current.uvi <= 2) {
-                $("#uvColor").css("background-color", "green").css("color", "white");
-            } else if (cityData.current.uvi >= 2 && cityData.current.uvi <= 5) {
-                $("#uvColor").css("background-color", "yellow").css("color", "black");
-            } else {
-                $("#uvColor").css("background-color", "red").css("color", "white");
-            }
-        }
-})
+}
+)
